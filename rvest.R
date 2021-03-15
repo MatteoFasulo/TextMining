39c6d60e796db649629d5eebae9cdbe32a8cc1f4
Matteo Fasulo <74818541+MatteoFasulo@users.noreply.github.com>
2021-03-15 21:11:00 +0100

    Update rvest.R

--- rvest.R
+++ rvest.R
@@ -1,49 +1,6 @@
 library(rvest)
 library(stringr)
 --------------------------------------------------------------------------------
-product_info <- function(url)
-{
-  url <- "https://www.amazon.it/Smartphone-Fotocamere-Posteriori-Espandibili-Batteria/dp/B087XGFYDW/ref=sr_1_9?__mk_it_IT=%C3%85M%C3%85%C5%BD%C3%95%C3%91&dchild=1&keywords=samsung&qid=1615487745&sr=8-9"
-  webdata <- read_html(url)
-  #webdata %>%
-  #html_nodes("p") %>%
-  #html_text()
-  title_html <- html_nodes(webdata, 'h1#title')
-  title <- html_text(title_html)
-  title <- str_replace_all(title, "[\r\n]" , "")
-  
-  price_html <- html_nodes(webdata, 'span#priceblock_ourprice')
-  price <- html_text(price_html)
-  price <- str_replace_all(price, "[\r\n]" , "")
-  
-  desc_html <- html_nodes(webdata, 'div#productDescription')
-  prod_desc <- html_text(desc_html)
-  prod_desc <- str_replace_all(prod_desc, "[\r\n\t]" , "")
-  prod_desc <- str_trim(prod_desc)
-  #head(prod_desc)
-  
-  color_html <- html_nodes(webdata, 'div#variation_color_name')
-  color_html <- html_nodes(color_html, 'span.selection')
-  color <- html_text(color_html)
-  color <- str_trim(color)
-  #head(color)
-  
-  rate_html <- html_nodes(webdata, 'span#acrPopover')
-  rate <- html_text(rate_html)
-  rate <- str_replace_all(rate, "[\r\n\t]" , "")
-  rate <- str_trim(rate)
-  rate <- rate[1]
-  
-  number_ratings <- html_nodes(webdata, 'span#acrCustomerReviewText')
-  number_ratings <- html_text(number_ratings)
-  number_ratings <- str_replace_all(number_ratings, "[\r\n\t]" , "")
-  number_ratings <- str_trim(number_ratings)
-  number_ratings <- number_ratings[1]
-  
-  result <- c(title, price, prod_desc, color, rate, number_ratings)
-  
-  return(result)
-}
 
 one_page_scraper <- function(url, page = 1, throttle = 0)
 {
@@ -58,16 +15,33 @@ one_page_scraper <- function(url, page = 1, throttle = 0)
   # Parse elements from HTML
   title <- doc %>%
     html_nodes("#cm_cr-review_list .a-color-base") %>%
-    html_text()
+    html_text() %>%
+    str_trim()
   
   author <- doc %>%
     html_nodes("#cm_cr-review_list .a-profile-name") %>%
-    html_text()
+    html_text() %>%
+    str_trim()
+  
+  state <- doc %>%
+    html_nodes("#cm_cr-review_list .review-date") %>%
+    html_text() %>% 
+    gsub(".*on ", "", .) %>%
+    str_remove_all(pattern = "Recensito ") %>%
+    str_remove_all(pattern = "in ") %>%
+    str_remove_all(pattern = " il ") %>%
+    str_remove_all(pattern = "nel ") %>%
+    str_remove_all(pattern = "negli ") %>%
+    str_remove_all(pattern = "[:digit:]") %>%
+    str_trim() 
+    state <- word(state, 1, -2)
   
   date <- doc %>%
     html_nodes("#cm_cr-review_list .review-date") %>%
     html_text() %>% 
-    gsub(".*on ", "", .)
+    gsub(".*on ", "", .) 
+    date <- word(date,5,-1) %>% 
+    str_remove_all(pattern = "il ")
   
   stars <- doc %>%
     html_nodes("#cm_cr-review_list  .review-rating") %>%
@@ -77,10 +51,12 @@ one_page_scraper <- function(url, page = 1, throttle = 0)
   
   comments <- doc %>%
     html_nodes("#cm_cr-review_list .review-text") %>%
-    html_text() 
+    html_text()  %>%
+    str_replace(pattern = "Your browser does not support HTML5 video.", replacement = "") %>%
+    str_trim()
   
   # Combine fields into data frame
-  df <- data.frame(title, author, date, stars, comments, stringsAsFactors = F)
+  df <- data.frame(title, author, state, date, stars, comments, stringsAsFactors = F)
   return(df)
 }
 
@@ -96,8 +72,4 @@ cycle_scraper <- function(product_id, to_page)
   return(reviews_all)
 }
 
-recensioni <- cycle_scraper(product_id = "B00IBDVTSU", to_page = 10)
-
-
-
-
+recensioni <- cycle_scraper(product_id = "B06XSC4H28", to_page = 35)
