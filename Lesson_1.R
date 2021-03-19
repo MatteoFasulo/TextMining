a9b6fa8c13c77573456da2ff7939a9fcfb470d2d
Matteo Fasulo <74818541+MatteoFasulo@users.noreply.github.com>
2021-03-19 09:39:43 +0100

    Update Lesson_1.R

--- Lesson_1.R
+++ Lesson_1.R
@@ -1,16 +1,18 @@
-############## INSTALL ##############
+############## INSTALL #########################################################
 install.packages("word2vec")
 install.packages("udpipe")
 install.packages("philentropy")
 install.packages("stopwords")
 install.packages("syuzhet")
-############## LOAD ##############
+install.packages("tm")
+############## LOAD ############################################################
 library(word2vec)
 library(udpipe)
 library(philentropy)
 library(stopwords)
 library(syuzhet)
-############## APPROACH ##############
+library(tm)
+############## APPROACH ########################################################
 data("brussels_reviews", package = "udpipe")
 x <- subset(brussels_reviews, language="es") #Not working actually
 which(x$language=="es")
@@ -19,7 +21,7 @@ xes <- tolower(xes)
 class(xes)
 model <- word2vec(x = xes, type = "skip-gram",dim = 15, iter = 20)
 vet_parola <- predict(model,"turismo",type = "embedding")
-############## NESPRESSO ##############
+############## NESPRESSO #######################################################
 nespresso <- read.csv(file = "20181110-inissia28_10.csv",sep = ";",stringsAsFactors = F)
 names(nespresso)
 class(nespresso)
@@ -43,4 +45,29 @@ unique(nespresso$Colour)
 table(nespresso$Colour)
 barplot(prop.table(table(nespresso$Colour)))
 colori <- as.matrix(table(nespresso$Colour))
-get_sentences(nespresso$Review.Text)
+nespresso_frasi <- get_sentences(nespresso$Review.Text)
+################################################################################
+nespressocorpus <- VCorpus(VectorSource(nespresso_frasi))
+stopwords("it")
+nespressopulito <- tm_map(nespressocorpus, removeWords, stopwords("it"))
+
+nespressopulito[[1]]$content
+nespresso_frasi[1] #removed "tutto"
+nespressopulito[[2]]$content
+
+nespresso2 <- tm_map(nespressocorpus, tolower) #lower
+nespresso2[[3318]]$content
+nespresso_frasi[3318]
+
+nespresso2 <- tm_map(nespressopulito, stripWhitespace) #no white spaces
+nespresso2[[2]]$content
+
+nespresso2 <- tm_map(nespressopulito, removePunctuation) #removed Punctuation
+nespresso2[[3315]]$content
+nespresso_frasi[3315]
+
+nespresso2 <- tm_map(nespressopulito, removeNumbers) #removed Numbers
+nespresso2[[3315]]$content
+
+nespresso2 <- tm_map(nespressopulito, stemDocument) #cambia le forme verbali
+nespresso2[[3300]]$content
